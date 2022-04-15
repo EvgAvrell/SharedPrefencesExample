7bad2edcf5df596dee7c641a74a68fb1596c0b7d
EvgEvgenu <jack11524524@mail.ru>
2022-04-15 13:21:43 +0400

    Working SharePrefences

--- app/src/main/java/com/example/sharedprefencesexample/MainActivity.java
+++ app/src/main/java/com/example/sharedprefencesexample/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.sharedprefencesexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText et;
+    Button btnSave, btnLoad;
+    SharedPreferences sharedPreferences;
+    final String SAVE_TEXT = "Save text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et = (EditText) findViewById(R.id.etText);
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnLoad = (Button) findViewById(R.id.btnLoad);
+
+        btnSave.setOnClickListener(this);
+        btnLoad.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnSave:
+                saveText();
+                break;
+            case R.id.btnLoad:
+                loadText();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void loadText() {
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        String savedText = sharedPreferences.getString(SAVE_TEXT, "Ничего не сохранено");
+        et.setText(savedText);
+        Toast.makeText(this, "Text loader", Toast.LENGTH_LONG).show();
+    }
+
+    private void saveText() {
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor ed = sharedPreferences.edit();
+        ed.putString(SAVE_TEXT, et.getText().toString());
+        ed.commit();
+        Toast.makeText(this, "Text save", Toast.LENGTH_LONG).show();
     }
 }
